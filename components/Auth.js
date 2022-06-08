d4b48f2e4bb686fa986dddee7d88ca5266c7a546
Koichi.Shimomura <unieye51@gmail.com>
2022-06-08 12:56:33 +0900

    i4-31 Auth component (Function)

--- components/Auth.js
+++ components/Auth.js
@@ -1,7 +1,66 @@
 /* eslint-disable @next/next/no-img-element */
-import { LockClosedIcon } from "@heroicons/react/solid";
+import { useState } from "react";
+import { useRouter } from "next/router";
+import Cookie from "universal-cookie";
 
-export default function Example() {
+const cookie = new Cookie();
+
+export default function Auth() {
+  const router = useRouter();
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [isLogin, setIsLogin] = useState(true);
+
+  const login = async () => {
+    try {
+      await fetch(`${process.env.NEXT_PUBLIC_RESTAPI_URl}api/auth/jwt/create`, {
+        method: "POST",
+        body: JSON.stringify({ username: username, password: password }),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      })
+        .then((res) => {
+          if (res.status === 400) {
+            throw "authentication failed";
+          } else if (res.ok) {
+            return res.json();
+          }
+        })
+        .then((data) => {
+          const options = { path: "/" };
+          cookie.set("access_token", data.access, options);
+        });
+      router.push("/main-page");
+    } catch (err) {
+      alert(err);
+    }
+  };
+
+  const authUser = async (e) => {
+    e.preventDefault();
+    if (isLogin) {
+      login();
+    } else {
+      try {
+        await fetch(`${process.env.NEXT_PUBLIC_RESTAPI_URl}api/register/`, {
+          method: "POST",
+          body: JSON.stringify({ username: username, password: password }),
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }).then((res) => {
+          if (res.status === 400) {
+            throw "authentication failed";
+          }
+        });
+        login();
+      } catch (err) {
+        alert(err);
+      }
+    }
+  };
+
   return (
     <div className="max-w-md w-full space-y-8">
       <div>
@@ -11,10 +70,15 @@ export default function Example() {
           alt="Workflow"
         />
         <h2 className="mt-6 text-center text-3xl font-extrabold text-white">
-          Sign in to your account
+          {isLogin ? "Login" : "Sign up"}
         </h2>
       </div>
-      <form className="mt-8 space-y-6" action="#" method="POST">
+      <form
+        className="mt-8 space-y-6"
+        action="#"
+        method="POST"
+        onSubmit={authUser}
+      >
         <input type="hidden" name="remember" defaultValue="true" />
         <div className="rounded-md shadow-sm -space-y-px">
           <div>
@@ -22,13 +86,16 @@ export default function Example() {
               Email address
             </label>
             <input
-              id="email-address"
-              name="email"
-              type="email"
-              autoComplete="email"
+              name="username"
+              type="text"
+              autoComplete="username"
               required
               className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
-              placeholder="Email address"
+              placeholder="Username"
+              value={username}
+              onChange={(e) => {
+                setUsername(e.target.value);
+              }}
             />
           </div>
           <div>
@@ -36,13 +103,16 @@ export default function Example() {
               Password
             </label>
             <input
-              id="password"
               name="password"
               type="password"
               autoComplete="current-password"
               required
               className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
               placeholder="Password"
+              value={password}
+              onChange={(e) => {
+                setPassword(e.target.value);
+              }}
             />
           </div>
         </div>
@@ -50,7 +120,7 @@ export default function Example() {
         <div className="flex items-center justify-center">
           <div className="text-sm">
             <span
-              href="#"
+              onClick={() => setIsLogin(!isLogin)}
               className="cursor-pointer font-medium text-white hover:text-indigo-500"
             >
               change mode ?
@@ -64,12 +134,21 @@ export default function Example() {
             className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
           >
             <span className="absolute left-0 inset-y-0 flex items-center pl-3">
-              <LockClosedIcon
+              <svg
                 className="h-5 w-5 text-indigo-500 group-hover:text-indigo-400"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 20 20"
+                fill="currentColor"
                 aria-hidden="true"
-              />
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z"
+                  clipRule="evenodd"
+                />
+              </svg>
             </span>
-            Sign in
+            {isLogin ? "Login with JWT" : "Create new user"}
           </button>
         </div>
       </form>
