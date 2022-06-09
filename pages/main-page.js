66f1d2dc75101e78cea01ec59b8819f40da85c2b
Koichi.Shimomura <unieye51@gmail.com>
2022-06-09 17:21:59 +0900

    4-33 Main page

--- pages/main-page.js
+++ pages/main-page.js
@@ -1,6 +1,7 @@
 import { useRouter } from "next/router";
 import Cookie from "universal-cookie";
 import Layout from "../components/Layout";
+import Link from "next/link";
 
 const cookie = new Cookie();
 
@@ -13,6 +14,19 @@ export default function MainPage() {
 
   return (
     <Layout title="Main page">
+      <div className="mb-10">
+        <Link href="/blog-page">
+          <a className="bg-indigo-500 mr-8  hover:bg-indigo-600 text-white px-4 py-12 rounded">
+            Visit Blog by SSG + ISR
+          </a>
+        </Link>
+        <Link href="/task-page">
+          <a className="bg-gray-500 ml-8 hover:bg-gray-600 text-white px-4 py-12 rounded">
+            Visit Task by ISR + CSR
+          </a>
+        </Link>
+      </div>
+
       <svg
         onClick={logout}
         className="mt-10 cursor-pointer w-6 h-6"
